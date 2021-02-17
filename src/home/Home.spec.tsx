530972b911024c4bab097a2932abceee60d39711
Samatar <axmedsamatar.dev@gmail.com>
2021-02-17 14:48:23 +0000

    Refactor tests and update circleci config

--- src/home/Home.spec.tsx
+++ src/home/Home.spec.tsx
@@ -1,34 +1,33 @@
-import { RenderResult, fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 import BaseApp from '../BaseApp'
 import { server } from '../mocks/server'
 
 describe('Home', () => {
-  let app: RenderResult
   beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
 
   beforeEach(() => {
-    app = render(<BaseApp />)
+    render(<BaseApp />)
   })
   afterEach(() => server.resetHandlers())
 
   afterAll(() => server.close())
 
   it('displays a count', () => {
-    app.getByText('0')
+    screen.getByText('0')
   })
 
   it('increments the count when the user clicks +', () => {
-    fireEvent.click(app.getByText('+'))
-    app.getByText('1')
+    fireEvent.click(screen.getByText('+'))
+    screen.getByText('1')
   })
 
   it('decrements the count when the user clicks -', () => {
-    fireEvent.click(app.getByText('-'))
-    app.getByText('-1')
+    fireEvent.click(screen.getByText('-'))
+    screen.getByText('-1')
   })
 
   it('makes a call to an api', async () => {
-    await app.findByText('Sam Ax')
+    await screen.findByText('Sam Ax')
   })
 })
